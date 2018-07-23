3fa91691957076b927b483b4d70ca84be6aa428a
Varun Keerthy <vkeerthy@btes.com.au>
2018-07-23 12:11:53 +1000

    Feature: Creation of second fragment in MainActivity

--- app/src/main/java/tutorial/demo/btes/com/andoridappfragments/MainActivity.java
+++ app/src/main/java/tutorial/demo/btes/com/andoridappfragments/MainActivity.java
@@ -3,11 +3,15 @@ package tutorial.demo.btes.com.andoridappfragments;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MenuDetailFragment frag = (MenuDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        frag.setMenuId(1);
     }
 }
--- app/src/main/java/tutorial/demo/btes/com/andoridappfragments/MenuDetailFragment.java
+++ app/src/main/java/tutorial/demo/btes/com/andoridappfragments/MenuDetailFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -28,7 +29,23 @@ public class MenuDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        if (savedInstanceState != null) {
+            menuId = savedInstanceState.getLong("menuId");
+        }
         return inflater.inflate(R.layout.fragment_menu_detail, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.name);
+            TextView desc = (TextView) view.findViewById(R.id.desc);
+            Menu menu = Menu.menus[(int) menuId];
+            title.setText(menu.getName());
+            desc.setText(menu.getDescription());
+
+        }
+    }
 }
